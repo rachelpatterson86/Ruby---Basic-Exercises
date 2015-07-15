e28a75e94018a801ca1585c406ec14fccb603618
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-07-15 05:03:32 -0400

    fizz buzz practice

---
+++ fizz_buzz.rb
@@ -0,0 +1,17 @@
+#if number is divisible by 3 print Fizz, if 5 print buzz if 3 & 5 print fizzbuzz
+num = 100
+def fizz_buzz(num)
+n=1
+  while n <= num do
+    if n%3 == 0 && n%5 == 0
+      puts "fizzbuzz"
+    elsif n%3 == 0
+      puts "fizz"
+    elsif n%5 == 0
+      puts "buzz"
+    else
+      puts n
+    end
+    n += 1
+  end
+end
